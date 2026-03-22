6349026b591fd7458c1488d75d28a14221dd7230
daynan f <dinuzeynu22@gmail.com>
2026-03-22 12:49:24 +0300

    Refactor A. FizzBuzz Remixed solution and add B. Robot Program problem files

--- A_FizzBuzz_Remixed.py
+++ A_FizzBuzz_Remixed.py
@@ -3,7 +3,10 @@ while t:
     t-=1
     x=int(input())
     cnt=0
-    for i in range(x+1):
+    y=x%15
+    n=x//15
+    for i in range(y+1):
         if i%3 == i%5:
             cnt+=1
-    print(cnt)
+    ans=3*n+cnt
+    print(ans)
