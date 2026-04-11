0d18204ef4e1dd28dd12f336f6dc26b3e0863f75
daynan f <dinuzeynu22@gmail.com>
2026-04-11 11:45:00 +0300

    solved a

--- A_Doremy_s_Paint_3.py
+++ A_Doremy_s_Paint_3.py
@@ -1,4 +1,18 @@
 t=int(input())
 while t:
     t-=1
-    
+    n=int(input())
+    nums=list(map(int,input().split()))
+    numsSet=set(nums)
+    if len(numsSet)<=1:
+        print("YES")
+    elif len(numsSet)==2:
+        from collections import Counter
+        freq = Counter(nums)
+        a,b=freq.values()
+        if a==b or abs(a-b)==1:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("NO")
