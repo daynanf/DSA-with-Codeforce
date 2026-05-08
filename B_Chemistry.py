3248b4a2a8834c91e4e5e29b91ca0dfcab1a57c4
daynan f <dinuzeynu22@gmail.com>
2026-05-08 12:16:24 +0300

    Add solution for Codeforces problem C. Vasilije in Cacak and refactor B. Chemistry solution

--- B_Chemistry.py
+++ B_Chemistry.py
@@ -1,4 +1,19 @@
 t=int(input())
 while t:
     t-=1
-    s,n=list(map(int,input().split()))
+    x,k=list(map(int,input().split()))
+    s=input()
+    freq=dict()
+    numOdds=numEvens=0
+    for x in s:
+        freq[x]=freq.get(x,0)+1
+    freqs=list(freq.values())
+    for x in freqs:
+        if x%2:
+            numOdds+=1
+        else:
+            numEvens+=1
+    if numOdds-1>k:
+        print("NO")
+    else:
+        print("YES")
---
+++ C_Vasilije_in_Cacak.py
@@ -0,0 +1,5 @@
+t=int(input())
+while t:
+    t-=1
+    n,k,x=list(map(int,input().split()))
+    
